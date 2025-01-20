0fb366401186e0e4f18e45f4de038aec4809be25
Vitalyi Drobot <vitalyi.drobot@yandex.ru>
2025-01-20 21:03:19 +0300

    Внесены исправления

--- scripts/DDL_tmp_sources_fact.sql
+++ scripts/DDL_tmp_sources_fact.sql
@@ -9,7 +9,7 @@ select
 	src.order_status,
 	current_timestamp
 from tmp_sources src
-join dwh.d_craftsman dc on dc.craftsman_name = src.craftsman_name and dc.craftsman_email = src.craftsman_email
-join dwh.d_customer dcs on dcs.customer_name=src.customer_name and dcs.customer_email=src.customer_email
-join dwh.d_product dp on dp.product_name = src.product_name and dp.product_description=src.product_description and 
+INNER join dwh.d_craftsman dc on dc.craftsman_name = src.craftsman_name and dc.craftsman_email = src.craftsman_email
+INNER join dwh.d_customer dcs on dcs.customer_name=src.customer_name and dcs.customer_email=src.customer_email
+INNER join dwh.d_product dp on dp.product_name = src.product_name and dp.product_description=src.product_description and 
 dp.product_price =src.product_price;
