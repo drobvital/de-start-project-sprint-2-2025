103a7a15ee01ba44e1bcf91033affca6459934ea
Vitalyi Drobot <vitalyi.drobot@yandex.ru>
2025-01-20 20:59:33 +0300

    Внесены исправления

--- scripts/DDL_tmp_sources.sql
+++ scripts/DDL_tmp_sources.sql
@@ -1,7 +1,77 @@
-drop table if exists tmp_sources;
-create temp table tmp_sources as
+DROP TABLE IF EXISTS tmp_sources;
+CREATE TEMP TABLE tmp_sources AS 
+    SELECT order_id,
+		order_created_date,
+		order_completion_date,
+		order_status,
+		craftsman_id,
+		craftsman_name,
+		craftsman_address,
+		craftsman_birthday,
+		craftsman_email,
+		product_id,
+		product_name,
+		product_description,
+		product_type,
+		product_price,
+		customer_id,
+		customer_name,
+		customer_address,
+		customer_birthday,
+		customer_email 
+  FROM source1.craft_market_wide
+UNION
+SELECT  t2.order_id,
+		t2.order_created_date,
+		t2.order_completion_date,
+		t2.order_status,
+		t1.craftsman_id,
+		t1.craftsman_name,
+		t1.craftsman_address,
+		t1.craftsman_birthday,
+		t1.craftsman_email,
+		t1.product_id,
+		t1.product_name,
+		t1.product_description,
+		t1.product_type,
+		t1.product_price,
+		t2.customer_id,
+		t2.customer_name,
+		t2.customer_address,
+		t2.customer_birthday,
+		t2.customer_email 
+  FROM source2.craft_market_masters_products t1 
+   INNER JOIN source2.craft_market_orders_customers t2 ON t2.product_id = t1.product_id and t1.craftsman_id = t2.craftsman_id 
+UNION
+SELECT  t1.order_id,
+		t1.order_created_date,
+		t1.order_completion_date,
+		t1.order_status,
+		t2.craftsman_id,
+		t2.craftsman_name,
+		t2.craftsman_address,
+		t2.craftsman_birthday,
+		t2.craftsman_email,
+		t1.product_id,
+		t1.product_name,
+		t1.product_description,
+		t1.product_type,
+		t1.product_price,
+		t3.customer_id,
+		t3.customer_name,
+		t3.customer_address,
+		t3.customer_birthday,
+		t3.customer_email
+  FROM source3.craft_market_orders t1
+   INNER JOIN source3.craft_market_craftsmans t2 ON t1.craftsman_id = t2.craftsman_id 
+   INNER JOIN source3.craft_market_customers t3 ON t1.customer_id = t3.customer_id
+union
 select
-	cpo.craftsman_id,
+	cpo.order_id,
+	cpo.order_created_date,
+	cpo.order_completion_date,
+	cpo.order_status,
+    cpo.craftsman_id,
 	cpo.craftsman_name,
 	cpo.craftsman_address,
 	cpo.craftsman_birthday,
@@ -11,14 +81,11 @@ select
 	cpo.product_description,
 	cpo.product_type,
 	cpo.product_price,
-	cpo.order_id,
-	cpo.order_created_date,
-	cpo.order_completion_date,
-	cpo.order_status,
 	c.customer_id,
 	c.customer_name,
 	c.customer_address,
 	c.customer_birthday,
 	c.customer_email 
 FROM external_source.craft_products_orders cpo
-join external_source.customers c on cpo.customer_id=c.customer_id; 
+inner join external_source.customers c on cpo.customer_id=c.customer_id;
+ 
